c53f7118a9fa44035e46c91312439953a475f8e6
Antonius Chang <antonius.changg@gmail.com>
2022-10-26 23:11:57 +0700

    Add missing seo title and description

--- pages/pokemon/[name].js
+++ pages/pokemon/[name].js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { getPokemonDetail, getPokemonList } from "../../services/pokemon";
 
@@ -7,37 +8,43 @@ export default function PokemonDetailPage({ pokemon }) {
   if(!pokemon) return <div>Loading...</div>;
 
   return (
-    <main>
-      <header>
-        <div className="cursor-pointer" onClick={() => router.push('/')}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
-        </div>
-      </header>
-      <img srcSet={pokemon.sprites.front_default} width={500} height="auto" />
-      <article>
-        <h1 className="text-5xl capitalize">{pokemon.name}</h1>
+    <div>
+      <Head>
+        <title>Pokedex - {pokemon.name} type and stats</title>
+        <meta name="description" content={`Pokemon ${pokemon.name} types, stats details. Find another Pokemon only on Pokedex!`} />
+      </Head>
+      <main>
+        <header>
+          <div className="cursor-pointer" onClick={() => router.push('/')}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+          </div>
+        </header>
+        <img srcSet={pokemon.sprites.front_default} width={500} height="auto" />
+        <article>
+          <h1 className="text-5xl capitalize">{pokemon.name}</h1>
 
-        <h2 className="text-2xl mt-5">Types</h2>
-        <div>
-          {pokemon.types.map(type => type.type.name).join(', ')}
-        </div>
+          <h2 className="text-2xl mt-5">Types</h2>
+          <div>
+            {pokemon.types.map(type => type.type.name).join(', ')}
+          </div>
 
-        <h2 className="text-2xl mt-5">Stats</h2>
-        <ul>
-          {pokemon.stats.map(stat => (
-            <li key={stat.stat.name}>
-              <div className="flex flex-row justify-between items-center">
-                <div>{stat.stat.name}</div>
-                <div>{stat.base_stat}</div>
-              </div>
-              <div>
-              <input className="w-full" type="range" value={stat.base_stat} disabled />
-              </div>
-            </li>
-          ))}
-        </ul>
-      </article>
-    </main>
+          <h2 className="text-2xl mt-5">Stats</h2>
+          <ul>
+            {pokemon.stats.map(stat => (
+              <li key={stat.stat.name}>
+                <div className="flex flex-row justify-between items-center">
+                  <div>{stat.stat.name}</div>
+                  <div>{stat.base_stat}</div>
+                </div>
+                <div>
+                <input className="w-full" type="range" value={stat.base_stat} disabled />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </main>
+    </div>
   )
 }
 
@@ -51,7 +58,7 @@ export async function getStaticProps({ params }) {
     props: {
       pokemon: data,
     },
-    revalidate: 30
+    revalidate: 15 * 60 // 15 minutes (maybe longer is better because supposedly pokemon data does not change that much)
   }
 }
 
